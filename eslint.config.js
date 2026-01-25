87121b22137236cac3c04e5bb6184811c5125eb4
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-01-25 08:04:52 +0100

    add react router and import sort package

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -20,4 +21,13 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+import './index.scss';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.scss';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
 import App from './App.tsx';
+import { AuthLayout } from './components/AuthLayout.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<AuthLayout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
